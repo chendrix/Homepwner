24d1618c9407a11dda1b5f26342a94506da30f32
Chris Hendrix <chris@pivotal.io>
2015-05-01 13:54:41 -0700

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by pivotal on 5/1/15.
+//  Copyright (c) 2015 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
