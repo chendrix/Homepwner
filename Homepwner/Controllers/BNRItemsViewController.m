ffe2c5c08d03805b737c4105b72f87695ed108fc
Chris Hendrix <cah276@cornell.edu>
2015-05-03 19:25:18 -0700

    Removing HeaderView and using UIBarButtonItems instead

--- Homepwner/Controllers/BNRItemsViewController.m
+++ Homepwner/Controllers/BNRItemsViewController.m
@@ -27,6 +27,11 @@
         UINavigationItem *navItem = self.navigationItem;
         
         navItem.title = @"Homepwner";
+        
+        UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
+        
+        navItem.rightBarButtonItem = bbi;
+        navItem.leftBarButtonItem = self.editButtonItem;
     }
     
     return self;
@@ -57,9 +62,6 @@
     [super viewDidLoad];
     
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
-    
-    UIView *headerView = self.headerView;
-    [self.tableView setTableHeaderView:headerView];
 }
 
 - (IBAction)addNewItem:(id)sender
@@ -71,26 +73,6 @@
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
     
     [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
-}
-
-- (IBAction)toggleEditingMode:(id)sender
-{
-    if (self.isEditing) {
-        [sender setTitle:@"Edit" forState:UIControlStateNormal];
-        [self setEditing:NO animated:YES];
-    } else {
-        [sender setTitle:@"Done" forState:UIControlStateNormal];
-        [self setEditing:YES animated:YES];
-    }
-}
-
-- (UIView *)headerView
-{
-    if (!_headerView) {
-       [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner: self options: nil];
-    }
-    
-    return _headerView;
 }
 
 - (NSArray *)allItems
